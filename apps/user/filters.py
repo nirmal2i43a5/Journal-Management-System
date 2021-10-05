cd7091ded5f66c98b2721f99740ee2fe8350267a
Nirmal <nirmalpandey048@gmail.com>
2021-10-05 13:07:25 +0545

    Modified home page articles design

--- apps/user/filters.py
+++ apps/user/filters.py
@@ -1,8 +1,12 @@
 import django_filters
+from django.utils.translation import gettext_lazy as _
 from .models import Article
+from apps.admin_user.models import Category
+
 
 class ArticleFilter(django_filters.FilterSet):
-    title = django_filters.CharFilter(lookup_expr='icontains')
+    title = django_filters.CharFilter(field_name = "title",lookup_expr='icontains')
+
     class Meta:
         model = Article
         fields = ['title', 'category']
