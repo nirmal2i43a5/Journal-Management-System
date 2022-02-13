bf1646065a8816c2edb17eda13a0688b48744ac5
nirmalpandey048@gmail.com <nirmalpandey048@gmail.com>
2022-02-13 14:03:01 +0545

    Updated previous design

--- apps/user/filters.py
+++ apps/user/filters.py
@@ -2,7 +2,8 @@ import django_filters
 from django.utils.translation import gettext_lazy as _
 from .models import Article
 from apps.admin_user.models import Category
-
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Row, Column
 
 class ArticleFilter(django_filters.FilterSet):
     title = django_filters.CharFilter(field_name = "title",lookup_expr='icontains')
@@ -10,3 +11,14 @@ class ArticleFilter(django_filters.FilterSet):
     class Meta:
         model = Article
         fields = ['title', 'category']
+        
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.helper = FormHelper()
+    #     self.helper.layout = Layout(
+    #         Row(
+    #             Column('title', css_class='form-group col-md-4 mb-0'),
+    #             Column('category', css_class='form-group col-md-4 mb-0'),
+    #             css_class='form-row'
+    #         ),
+    #     )  
